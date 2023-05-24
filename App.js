a636ba159f9af9ad70a1b5442e6438a17b297916
ValentinBailleulGH <valentin.bailleul@edu.esiee.fr>
2023-05-24 18:23:25 +0200

    added SelectAge & SelectGender

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-import { Text, View, Image } from 'react-native'
+import { Text, View, Image, Switch } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import React from 'react'
+import React, { useState } from 'react'
 import { Ionicons } from '@expo/vector-icons'
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
+import Slider from '@react-native-community/slider'
+import { Picker } from '@react-native-picker/picker'
+
 // #region Setup
-// const styles = {
-//   flexCenter: { display: 'flex', alignItems: 'center', justifyContent: 'center' }
-// }
+const styles = {
+  flexCenter: { display: 'flex', alignItems: 'center', justifyContent: 'center' },
+  settingsTitle: { fontSize: 18 }
+}
 
 const MainView = ({ children }) => {
   return (
@@ -38,7 +42,7 @@ const ViewsHandler = () => {
           options={{
             tabBarLabel: 'Food Database',
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="fast-food-outline" color={color} size={size} />
+            <Ionicons name="fast-food-outline" color={color} size={size} />
             )
           }}
         />
@@ -120,12 +124,56 @@ const ProfileScreen = () => {
   return (
     <MainView>
       <View>
-        <Text>Profile Screen</Text>
+        <ProfileForm />
       </View>
     </MainView>
   )
 }
 // #endregion
+
+const SelectAge = ({ age, setAge }) => {
+  return (
+    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={styles.settingsTitle}>{age ? `Age: ${age}` : 'Select your age'}</Text>
+        <Slider
+          style={{ width: 200, height: 40 }}
+          minimumValue={15}
+          maximumValue={80}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#000000"
+          onValueChange={(newValue) => setAge(newValue.toFixed(0))}
+        />
+      </View>
+  )
+}
+const SelectGender = ({ gender, setGender }) => {
+  return (
+    <View style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={styles.settingsTitle}>{gender && gender !== '-' ? `Gender: ${gender}` : 'Select your gender'}</Text>
+        <Picker style={{ width: 200 }}
+          selectedValue={gender}
+          onValueChange={(v) => setGender(v)}
+        >
+          <Picker.Item label=" " value="-" />
+          <Picker.Item label="Male" value="Male" />
+          <Picker.Item label="Female" value="Female" />
+          <Picker.Item label="Other" value="Other" />
+        </Picker>
+    </View>
+  )
+}
+
+const ProfileForm = () => {
+  const [age, setAge] = useState(undefined)
+  const [gender, setGender] = useState(undefined)
+
+  return (
+    <View style={{ display: 'flex', alignItems: 'center', backgroundColor: 'green', paddingTop: 20, minHeight: '100%' }}>
+      <SelectAge age={age} setAge={setAge}/>
+      <SelectGender gender={gender} setGender={setGender} />
+    </View>
+  )
+}
 
 export default function App () {
   return (
