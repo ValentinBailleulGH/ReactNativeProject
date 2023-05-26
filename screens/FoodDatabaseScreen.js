0cd6c791186bf837f2f8052eaf1dac707a7cd780
dasilvar <remi.dasilva@edu.esiee.fr>
2023-05-26 15:03:34 +0200

    feat: fooddatabase

--- screens/FoodDatabaseScreen.js
+++ screens/FoodDatabaseScreen.js
@@ -1,70 +1,74 @@
 import { View, FlatList } from 'react-native'
-import { Text, Searchbar, Card, List  } from 'react-native-paper';
+import { Text, Searchbar, Card, List } from 'react-native-paper'
 import React from 'react'
 
 import MainView from '../components/MainView'
-import FoodApiService from '../services/FoodApiService';
+import FoodApiService from '../services/FoodApiService'
 
 export default function FoodDatabaseScreen ({ route }) {
-  const [foodNameToSearch, setFoodNameToSearch] = React.useState(undefined);
-  const [hintResults, setHintsResults] = React.useState([]);
+  const [foodNameToSearch, setFoodNameToSearch] = React.useState('')
+  const [hintResults, setHintsResults] = React.useState([])
 
   const handlePressSearchFoodIcon = () => {
-    if(!foodNameToSearch){
-      return;
+    if (!foodNameToSearch) {
+      return
     }
-    
+
     FoodApiService.getFoods(foodNameToSearch)
-    .then((response) => {
-      setHintsResults(response.hints);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+      .then((response) => {
+        setHintsResults(response.hints)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
   }
 
-  const renderFood = ({label: foodLabel, nutrients: foodNutrients}) => {
-      const LABELS_NUTRIMENTS = [
-        "Calories/Energie",
-        "Protéines",
-        "Lipides",
-        "Glucides",
-        "Fibre alimentaire"
-      ]
-    
-      return(
-        <Card>
-          <Card.Title title={foodLabel} />
-          <Card.Content>
-            {Object.values(foodNutrients).map((nutrimentValue, index) => {
-              <List.Item 
+  const renderFood = ({ label: foodLabel, nutrients: foodNutrients }) => {
+    const LABELS_NUTRIMENTS = [
+      'Calories/Energie',
+      'Protéines',
+      'Lipides',
+      'Glucides',
+      'Fibre alimentaire'
+    ]
+
+    return (
+      <Card>
+        <Card.Title title={foodLabel} />
+        <Card.Content>
+          {Object.values(foodNutrients).map((nutrimentValue, index) => {
+            return (
+              <List.Item
+                key={index}
                 title={`${LABELS_NUTRIMENTS[index]} : ${nutrimentValue}`}
               />
-            })
-            }
-          </Card.Content>
-        </Card>
-      )
+            )
+          })}
+        </Card.Content>
+      </Card>
+    )
   }
 
   return (
-      <MainView>
+    <MainView>
+      <View>
+        <Text variant="displaySmall">Rechercher un aliment</Text>
         <View>
-          <Text variant="displaySmall">Rechercher un aliment</Text>
-          <View>
-            <Searchbar  
-              label="Chercher un aliment"
-              value={foodNameToSearch}
-              onChangeText={(newFoodNameToSearch) => setFoodNameToSearch(newFoodNameToSearch)}
-              onIconPress={handlePressSearchFoodIcon}
-            />
-            <FlatList
+          <Searchbar
+            label="Chercher un aliment"
+            value={foodNameToSearch}
+            onChangeText={(newFoodNameToSearch) =>
+              setFoodNameToSearch(newFoodNameToSearch)
+            }
+            onIconPress={handlePressSearchFoodIcon}
+          />
+          <FlatList
               data={hintResults}
-              renderItem={(item) => {console.log('item : ', item)}}
-              keyExtractor={({food}) => {food.foodId}}
+              renderItem={({ item: { food } }) => renderFood(food)}
+              keyExtractor={({ food }) => `${food.foodId}-${food.label}` }
             />
-          </View>
         </View>
-      </MainView>
+      </View>
+    </MainView>
   )
 }
--- services/FoodApiService.js
+++ services/FoodApiService.js
@@ -1,59 +1,53 @@
-const APP_ID = "0022e75e";
-const APP_KEY = "509117406bd888ad296d7498443d96f8";
-
+const APP_ID = '0022e75e'
+const APP_KEY = '509117406bd888ad296d7498443d96f8'
 
 export default {
-    /**
-     * 
-     * @param {String} method 
-     * @param {String} ressouces 
-     * @param {Array} urlParams 
-     * @param {Object} data 
-     * @returns 
-     */
-    async call(method, ressouces, urlParams, data = null) {
-        const HEADERS = {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        };
-
-        const BASE_URL = 'https://api.edamam.com/api/food-database/v2/'
-
-        const urlParamsFormated = urlParams.map(({ name, value }) => {
-            return `${name}=${value}`
-        })
-
-        const URL = `${BASE_URL}${ressouces}?app_id=${APP_ID}&app_key=${APP_KEY}${urlParamsFormated.len ? '&' + urlParamsFormated : ''}`
-        return fetch(
-            URL,
-            {
-                method: method,
-                headers: HEADERS,
-                body: data
-            }
-        ).then(async (response) => {
-            return response.json()
-                .then((json) => {
-                    return Promise.resolve(json);
-                }
-                )
-                .catch((error) => {
-                    Promise.reject(error);
-                }
-                )
-        }
-        ).catch((error) => {
-            Promise.reject(error);
-        }
-        )
-    },
-    getFoods(ingredient) {
-        return this.call(
-            'GET',
-            'parser',
-            [
-                { 'name': 'ingr', 'value': ingredient }
-            ]
-        )
+  /**
+   *
+   * @param {String} method
+   * @param {String} ressouces
+   * @param {Array} urlParams
+   * @param {Object} data
+   * @returns
+   */
+  async call (method, ressouces, urlParams, data = null) {
+    const HEADERS = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
     }
-} 
+
+    const BASE_URL = 'https://api.edamam.com/api/food-database/v2/'
+
+    const urlParamsFormated = urlParams.map(({ name, value }) => {
+      return `${name}=${value}`
+    })
+
+    const URL = `${BASE_URL}${ressouces}?app_id=${APP_ID}&app_key=${APP_KEY}${
+      urlParamsFormated.length > 0 ? '&' + urlParamsFormated : ''
+    }`
+
+    console.log(URL)
+
+    return fetch(URL, {
+      method,
+      headers: HEADERS,
+      body: data
+    })
+      .then(async (response) => {
+        return response
+          .json()
+          .then((json) => {
+            return Promise.resolve(json)
+          })
+          .catch((error) => {
+            Promise.reject(error)
+          })
+      })
+      .catch((error) => {
+        Promise.reject(error)
+      })
+  },
+  getFoods (ingredient) {
+    return this.call('GET', 'parser', [{ name: 'ingr', value: ingredient }])
+  }
+}
