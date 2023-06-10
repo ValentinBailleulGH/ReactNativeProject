cca9ba74dfec7629f7a845b919031ff2b844ff9d
DaSilvaRemi <remi.dasilva.dev@gmail.com>
2023-06-10 18:33:50 +0200

    styles

--- components/MealFoodsDisplay.js
+++ components/MealFoodsDisplay.js
@@ -3,7 +3,7 @@ import { List } from 'react-native-paper'
 import { Ionicons } from '@expo/vector-icons'
 import globalStyles from '../styles'
 
-export default function MealFoodsDisplay ({ mealPlan, data, handleDeleteFood }) {
+export default function MealFoodsDisplay({ mealPlan, data, handleDeleteFood }) {
   return (
     <List.Accordion id={mealPlan} key={mealPlan} title={mealPlan}>
       {data.map((food, index) => (
@@ -12,7 +12,19 @@ export default function MealFoodsDisplay ({ mealPlan, data, handleDeleteFood }) 
           key={`${food.label}-${index}`}
           title={`${food.label} (${food.quantity}g)`}
           // right={(props) => <IconButton iconColor='red' icon="delete" onPress={() => handleDeleteFood(mealPlan, food)} />}
-          right={() => <Ionicons style={{ backgroundColor: globalStyles.colors.SecondaryButton, borderRadius: 10, paddingHorizontal: 10 }} color={'red'} name="trash-bin" size={20} onPress={() => handleDeleteFood(mealPlan, food)} />}
+          right={() => (
+            <Ionicons
+              style={{
+                backgroundColor: globalStyles.colors.SecondaryButton,
+                borderRadius: 10,
+                paddingHorizontal: 10
+              }}
+              color={'red'}
+              name="trash-bin"
+              size={20}
+              onPress={() => handleDeleteFood(mealPlan, food)}
+            />
+          )}
         />
       ))}
     </List.Accordion>
